a8cebdfea14b41e72792dbfbf29abca4fac25269
Shawn Marema <hillhike72@yahoo.com>
2021-04-25 09:37:07 -0500

    Initial load

---
+++ Sorting.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+//Purpose of this playground is to practice the sort algorithms including the following but not limited to
+//Insertion sort
+//Merge sort
+//Heapsort
+//Quicksort
+//Counting sort
+//Radix sort
+//Bucket sort
+
+//After writting and testing code for each write program to compare the times of sort for each algorithms; maybe even have them race to finish
+
+//Gotta start somewhere
+var str = "Hello, playground"
+print (str)
+
+//************************
+//*****Insertion Sort*****
+//************************
+
